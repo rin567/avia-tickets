59d8ce499d9f3bc46661196cea30f92a9f7871d7
rin567 <ivakatya5@gmail.com>
2022-10-23 20:55:36 +0300

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,41 @@
+import '../css/style.css';
+import locations from './store/locations';
+import './plugins';
+import formUI from './views/form';
+import currencyUI from './views/currency';
+import ticketsUI from './views/tickets';
+
+document.addEventListener('DOMContentLoaded', () => {
+  initApp();
+  const form = formUI.form;
+
+  // Events
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    onFormSubmit();
+  })
+
+  // Heandlers
+  async function initApp() {
+    await locations.init();
+    formUI.setAutocompleteData(locations.shortCitiesList);
+  } 
+
+  async function onFormSubmit() {
+    const origin = locations.getCityCodeByKey(formUI.originValue);
+    const destination = locations.getCityCodeByKey(formUI.destinationValue);;
+    const depart_date = formUI.departDateValue;
+    const return_date = formUI.returnDateValue;
+    const currency = currencyUI.currencyValue;
+
+    await locations.fetchTickets({
+      origin,
+      destination,
+      depart_date,
+      return_date,
+      currency,
+    })
+
+    ticketsUI.renderTickets(locations.lastSearch);
+  }
+})
---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry:{
+    polyfill: 'babel-polyfill',
+    app: './js/app.js'
+  },
+
+  context: path.resolve(__dirname, 'src'),
+
+  devServer: {
+    port:9000,
+    host: 'localhost',
+    hot: true,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+        test: /\.js$/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+
+            options: {
+              importLoaders: 1,
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              // plugins: () => [precss, autoprefixer],
+              postcssOptions: {
+                plugins: [require('postcss-preset-env')],
+              }
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: './style.css'
+    }),
+  ],
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].[contenthash].js',
+  },
+
+  mode: 'development',
+
+}
